01fc8ca502409a14846b1aff4dec25c8734853a5
franknfjr <frankdonascimentojr@gmail.com>
2021-04-21 16:46:24 -0300

    first commit

---
+++ AnaninPlaces/AnaninPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnaninPlacesApp.swift
+//  AnaninPlaces
+//
+//  Created by Frank on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct AnaninPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
