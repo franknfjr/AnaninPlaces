769d7815324a68e4ec36f5bc6e32140d635c9c7d
franknfjr <frankdonascimentojr@gmail.com>
2021-04-21 17:17:42 -0300

    basic story

--- AnaninPlaces/ContentView.swift
+++ AnaninPlaces/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                AnaninCategoriesView()
+            }.navigationTitle("AnaninPlaces")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AnaninCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0..<10, id: \.self) { num in
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.blue)
+                            .cornerRadius(.infinity)
+                        Text("Art")
+                            .font(.system(size: 12, weight:.semibold))
+                    }
+                }
+            }.padding(.horizontal)
+        }
     }
 }
