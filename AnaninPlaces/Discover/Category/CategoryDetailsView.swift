80d112938728944b29f2995e6b7bad860ccc3d51
franknfjr <frankdonascimentojr@gmail.com>
2021-04-29 23:37:42 -0300

    move logic server to block correct

--- AnaninPlaces/Discover/Category/CategoryDetailsView.swift
+++ AnaninPlaces/Discover/Category/CategoryDetailsView.swift
@@ -25,14 +25,14 @@ class CategoryDetailsViewModel: ObservableObject {
         }
         
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
-            
-            if let statusCode = (resp as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
-                self.isLoading = false
-                self.errorMessage = "Bad status: \(statusCode)"
-                return
-            }
-            
+                        
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                
+                if let statusCode = (resp as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
+                    self.isLoading = false
+                    self.errorMessage = "Bad status: \(statusCode)"
+                    return
+                }
                 
                 guard let data = data else { return }
                 
